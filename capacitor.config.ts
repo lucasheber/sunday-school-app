6121a5a950bf0dfc4beeabc95a1868b5679f6fd6
Lucas Heber <lucas.heber07@gmail.com>
2025-04-21 22:54:20 -0300

    feat(config): update app configuration and add splash screen and status bar plugins

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,28 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'sunday-school-app',
-  webDir: 'www'
+  appId: 'ebd.lucasheber.dev',
+  appName: 'EBD ADPTB',
+  webDir: 'www',
+
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      showSpinner: false,
+      backgroundColor: '#87d7f3',
+      androidSpinnerStyle: 'small',
+      iosSpinnerStyle: 'small',
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+    StatusBar: {
+      backgroundColor: '#000000',
+      style: 'DARK',
+      show: true,
+      color: '#000000',
+      overlay: true,
+    },
+  }
 };
 
 export default config;
