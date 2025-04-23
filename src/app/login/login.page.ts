a68224455072f234aec7387012c748d9797c1362
Lucas Heber <lucas.heber07@gmail.com>
2025-04-23 09:50:32 -0300

    feat(login): add validation and error messages for email and password fields

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonContent, IonToolbar, IonButton, IonItem, IonIcon, IonInput, IonFooter, IonSelect, IonSelectOption } from '@ionic/angular/standalone';
 import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -16,6 +19,10 @@ export class LoginPage implements OnInit {
   screen: any = 'signin';
   formData: FormGroup;
   isLoading: boolean = false;
+  invalidCredentials: boolean = false;
+
+  private readonly router = inject(Router);
+  private readonly authenticationService = inject(AuthService);
 
   constructor(private fb: FormBuilder, private translocoService: TranslocoService) {
     this.formData = this.fb.group({
@@ -25,7 +32,16 @@ export class LoginPage implements OnInit {
     });
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.authenticationService.isLoggedIn$().pipe(
+      tap((isLoggedIn) => {
+        if (isLoggedIn) {
+          this.router.navigate(['/home']);
+        }
+      })
+    ).subscribe();
+
+  }
 
   change(event: any) {
     this.screen = event;
@@ -33,15 +49,55 @@ export class LoginPage implements OnInit {
 
   login() {
     var formData: any = new FormData();
+
+    // remove the name field from the formData
+    this.formData.removeControl('name');
+
+    // validate email
+    if (this.formData.get('email')?.value.length < 1) {
+      this.formData.get('email')?.setErrors({ 'required': true });
+      this.formData.get('email')?.markAsTouched();
+      this.formData.get('email')?.markAsDirty();
+      this.formData.get('email')?.updateValueAndValidity();
+    }
+
+    // validate password
+    if (this.formData.get('password')?.value.length < 6) {
+      this.formData.get('password')?.setErrors({ 'minlength': true });
+      this.formData.get('password')?.markAsTouched();
+      this.formData.get('password')?.markAsDirty();
+      this.formData.get('password')?.updateValueAndValidity();
+    }
+
+
     if (this.formData.valid) {
       this.isLoading = true
-      formData.append('email', this.formData.get('email')?.value ?? '');
-      formData.append('password', this.formData.get('password')?.value ?? '');
-      console.log(this.formData)
-      // this.auth.userLogin(formData).subscribe((data: any) => {
-      //   console.log(data);
-      // });
+      const email = this.formData.get('email')?.value;
+      const password = this.formData.get('password')?.value;
+
+      formData.append('email', email ?? '');
+      formData.append('password', password ?? '');
+
+      this.authenticationService
+        .login(email, password)
+        .subscribe((data: any) => {
+          console.log('login', data);
+          this.isLoading = false;
+          if (data) {
+            this.router.navigate(['/home']);
+          }
+        }, (error: any) => {
+          console.log('error', error);
+          this.isLoading = false;
+          this.invalidCredentials = true;
+          setTimeout(() => {
+            this.invalidCredentials = false;
+          }
+            , 3000);
+        }
+        );
     }
+
   }
 
   register() {
