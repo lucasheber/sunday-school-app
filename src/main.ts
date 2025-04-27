0753258425d23c93be5762b9f09b79606dc16ce7
Lucas Heber <lucas.heber07@gmail.com>
2025-04-27 16:21:22 -0300

    feat(service-worker): add service worker support and update manifest for PWA

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ import { provideFirebaseApp, initializeApp,  } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';``
 import { environment } from './environments/environment';
+import { provideServiceWorker } from '@angular/service-worker';
 
 
 bootstrapApplication(AppComponent, {
@@ -33,6 +34,9 @@ bootstrapApplication(AppComponent, {
     }),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
+    provideFirestore(() => getFirestore()), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 });
