cf436a2469128fabc87600d3791a649f269d9754
Lucas Heber <lucas.heber07@gmail.com>
2025-04-30 16:35:31 -0300

    feat(app): initialize status bar settings for Android platform

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { StatusBar, Style } from '@capacitor/status-bar';
+import { isPlatform } from '@ionic/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,19 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+
+  constructor() { }
+
+  async ngOnInit() {
+    await this.setStatusBar();
+  };
+
+  async setStatusBar() {
+    if (isPlatform('android')) {
+      await StatusBar.setBackgroundColor({ color: '#ffffff' }); // cor do header
+      await StatusBar.setOverlaysWebView({ overlay: true });
+      await StatusBar.setStyle({ style: Style.Dark });
+      await StatusBar.show();
+    }
+  }
 }
