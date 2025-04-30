7db984026818a362f030c071472e2246be43342f
Lucas Heber <lucas.heber07@gmail.com>
2025-04-30 16:35:53 -0300

    feat(classes): enhance class listing with dynamic data and improved layout

--- src/app/pages/classes/classes.page.ts
+++ src/app/pages/classes/classes.page.ts
@@ -1,42 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonMenuButton, IonButtons, IonList, IonItem, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonMenuButton, IonButton, IonButtons, IonList, IonItem, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonGrid, IonRow, IonCol, IonChip, IonIcon } from '@ionic/angular/standalone';
 import { TranslocoModule } from '@jsverse/transloco';
+import { Observable } from 'rxjs';
+import { collection } from 'firebase/firestore';
+import { collectionData, Firestore } from '@angular/fire/firestore';
+import { Class } from 'src/models/class';
+import { addIcons } from 'ionicons';
+import { people, peopleOutline, personOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-classes',
   templateUrl: './classes.page.html',
   styleUrls: ['./classes.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonMenuButton, IonButtons, IonList, IonItem, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle, CommonModule, FormsModule, TranslocoModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonMenuButton, IonButton, IonButtons, IonList, IonItem, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonGrid, IonRow, IonCol, IonChip, IonIcon, CommonModule, FormsModule, TranslocoModule]
 })
 export class ClassesPage implements OnInit {
 
-  public readonly classes = [
-    {
-      'name': 'Math',
-      'description': 'Learn about numbers and equations',
-    },
-    {
-      'name': 'Science',
-      'description': 'Explore the wonders of the universe',
-    },
-    {
-      'name': 'History',
-      'description': 'Discover the past and its impact on the present',
-    },
-    {
-      'name': 'Art',
-      'description': 'Express yourself through creativity and imagination',
-    },
-  ]
+  public readonly classes$: Observable<Class[]>;
 
-
-  constructor() { }
-
+  constructor(private firestore: Firestore) {
+    const classesCollection = collection(this.firestore, 'classes');
+    this.classes$ = collectionData(classesCollection, { idField: 'id' }) as Observable<any[]>;
+  }
 
   ngOnInit() {
+    addIcons({ personOutline, peopleOutline })
   }
 
 }
---
+++ src/models/class.ts
@@ -0,0 +1,6 @@
+export interface Class {
+    id: string;
+    name: string;
+    category: string;
+    color: string;
+}
